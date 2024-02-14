b9e0056ffbef101f3934615b013a72bcd264ab10
elbadcodE <lbatvt@gmail.com>
2024-02-14 10:43:09 -0500

    minor annoyance removal
    instead of annoying user with an error message when the process is running and minimized simply close it and continue with starting new process

--- unlockfps_nc/Program.cs
+++ unlockfps_nc/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
+using unlockfps_nc.Model;
 using unlockfps_nc.Service;
 using unlockfps_nc.Utility;
 
@@ -11,28 +13,36 @@ namespace unlockfps_nc
         public static IServiceProvider ServiceProvider { get; private set; }
 
         [STAThread]
-        static void Main()
+        static void Main ( )
         {
-            MutexHandle = Native.CreateMutex(IntPtr.Zero, true, @"GenshinFPSUnlocker");
-            if (Marshal.GetLastWin32Error() == 183)
+            Process existingProcess = null;
+            MutexHandle = Native.CreateMutex ( IntPtr.Zero, true, @"GenshinFPSUnlocker" );
+            if ( Marshal.GetLastWin32Error ( ) == 183 )
             {
-                MessageBox.Show(@"Another unlocker is already running.", @"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-            
-            var services = new ServiceCollection();
-            services.AddTransient<MainForm>();
-            services.AddTransient<SettingsForm>();
-            services.AddTransient<SetupForm>();
-            services.AddSingleton<ConfigService>();
-            services.AddSingleton<ProcessService>();
+                // Find the existing process
+                existingProcess = Process.GetProcessesByName ( "unlockfps_nc" ).FirstOrDefault ( );
+                if (existingProcess != null ) { 
+                   
+                    existingProcess.MainModule.Dispose ( );
+                    existingProcess.CloseMainWindow ( );
+                }
+             }
 
-            ServiceProvider = services.BuildServiceProvider();
 
-            ApplicationConfiguration.Initialize();
-            Application.Run(ServiceProvider.GetRequiredService<MainForm>());
+                var services = new ServiceCollection ( );
+                services.AddTransient<MainForm> ( );
+                services.AddTransient<SettingsForm> ( );
+                services.AddTransient<SetupForm> ( );
+                services.AddSingleton<ConfigService> ( );
+                services.AddSingleton<ProcessService> ( );
+
+                ServiceProvider = services.BuildServiceProvider ( );
+
+                ApplicationConfiguration.Initialize ( );
+                Application.Run ( ServiceProvider.GetRequiredService<MainForm> ( ) );
+
         }
+    }
 
 
     }
-}
